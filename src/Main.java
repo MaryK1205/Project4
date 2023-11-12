b2c8a468f36abd300082cc8ba4eb5420d1ecc933
Мария <kolesnikovamaria169@gmail,com>
2023-11-12 20:50:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        CredCalculator service = new CredCalculator();
+        System.out.println(service.calculate(1000000, 9.99, 1));
+        System.out.println(service.calculate(1000000, 9.99, 2));
+        System.out.println(service.calculate(1000000, 9.99, 3));
+    }
+}
