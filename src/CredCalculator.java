34eac5e66122febb09cda034a2874739c52a6f85
Мария <kolesnikovamaria169@gmail,com>
2023-11-13 11:01:13 +0200

    Initial commit

--- src/CredCalculator.java
+++ src/CredCalculator.java
@@ -1,9 +1,9 @@
 public class CredCalculator {
-      public double calculate(int sumKred, double rateYear, int numYear) {
-            double rateMonth = rateYear/100/12;
-            double constant =  Math.pow((1 + rateMonth), (numYear*12));
-            double payment = sumKred * (rateMonth * constant)/(constant - 1);
-            return (int) payment;
+    public double calculate(int sumKred, double rateYear, int numYear) {
+        double rateMonth = rateYear / 100 / 12;
+        double constant = Math.pow((1 + rateMonth), (numYear * 12));
+        double payment = sumKred * (rateMonth * constant) / (constant - 1);
+        return (int) payment;
 
     }
 }
